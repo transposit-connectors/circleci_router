22edbfac4ec2119ce18c3eadae3ec4d1d4c2cedd
Nina Yang <nina@transposit.com>
2019-06-07 16:35:24 +0000

    Commit from Transposit IDE

---
+++ src/webhook.js
@@ -0,0 +1,13 @@
+({ http_event }) => {
+  api.log(http_event);
+  return {
+    status_code: 200,
+    headers: { "Content-Type": "application/json" },
+    body: { "greeting": "Hello World" }
+  };
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/building/webhooks
+ */
