dd63523a780f79d104e8d73509135e0b7f939b3e
Nina Yang <nina@transposit.com>
2019-07-19 00:53:23 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -9,7 +9,7 @@
   body.channel = params.channel;
   body.channel = "CK4K2AK4N";
   
-  api.log(api.user({type: "slack"}));
+  api.log(api.user({type: "slack", workspaceId: "T2615V5UK"}));
   setImmediate(() => {
     try {
       var textArr = body.attachments[0].text.split(" ");
